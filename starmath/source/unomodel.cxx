599f41b822d1cde14e65f3687ae7f44098a652a7
Michael Meeks <michael.meeks@collabora.com>
2015-03-16 21:50:14 +0000

    fix thinko in starmath; time to sleep.
    Change-Id: I6846e56062fdde394e4039de7cde42b0560f34b3

--- starmath/source/unomodel.cxx
+++ starmath/source/unomodel.cxx
@@ -1037,8 +1037,8 @@ void SAL_CALL SmModel::render(
     if (xRenderDevice.is())
     {
         VCLXDevice*   pDevice = VCLXDevice::GetImplementation( xRenderDevice );
-        VclRef< OutputDevice> pOut = pDevice ? pDevice->GetOutputDevice()
-                                             : VclRef< OutputDevice >();
+        VclPtr< OutputDevice> pOut = pDevice ? pDevice->GetOutputDevice()
+                                             : VclPtr< OutputDevice >();
         if (!pOut)
             throw RuntimeException();
 
